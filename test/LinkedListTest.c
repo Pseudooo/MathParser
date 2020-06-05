1c107df6d23e1ad6e5c1da9e37d383b30b2cec73
pseudo <mitchell.wright877@gmail.com>
2020-06-05 19:49:53 +0100

    flush

--- test/LinkedListTest.c
+++ test/LinkedListTest.c
@@ -6,11 +6,15 @@
 #include "Test.h"
 
 void test_append();
+void test_push();
+void test_pop();
 
 int main()
 {
 
 	test_append();
+	test_push();
+	test_pop();
 
 }
 
@@ -24,18 +28,79 @@ void test_append()
 	LinkedList* list = ll_init();
 	size_t elem_size = 4;
 
-	// Attempt to `append` 64 values
+	// Attempt to `append` values
 	for(int i = 0; i < n; i++)
 	{
+		// Append a value to list
 		int val = i*i;
 		ll_append(list, &val, elem_size);
 		
-		// Ensure list is 
+		// Test
 		assert_eq_int(val, * (int*) list->tail->payload);
 		assert_eq_int(i + 1, list->length);
 	}
 
 	// Clear list from memory
+	ll_dispose(list);
+	terminate_test_suite();
+
+}
+
+void test_push()
+{
+
+	const int n = 16;
+	init_test_suite(n * 2, "Push", "Testing Functionality of pushing values to front of list");
+
+	// Initialize
+	LinkedList* list = ll_init();
+	size_t elem_size = 4;
+
+	// Attempt to `push` values
+	for(int i = 0; i < n; i++)
+	{
+
+		// Push value
+		int val = i*i;
+		ll_push(list, &val, elem_size);
+
+		// Test
+		assert_eq_int(val, * (int*) list->head->payload);
+		assert_eq_int(i + 1, list->length);
+
+	}
+
+	ll_dispose(list);
+	terminate_test_suite();
+
+}
+
+void test_pop()
+{
+
+	const int n = 16;
+	init_test_suite(2 * n, "Pop", "Testing functionality of popping values from the front of list");
+
+	// init
+	LinkedList* list = ll_init();
+	size_t elem_size = 4;
+
+	// Populate list
+	for(int i = 0; i < n; i++)
+	{
+		int val = i * i;
+		ll_append(list, &val, elem_size);
+	}
+
+	for(int i = 0; i < n; i++)
+	{
+		int out;
+		ll_pop(list, &out);
+
+		assert_eq_int(i*i, out);
+		assert_eq_int(n - i - 1, list->length);
+	}
+
 	ll_dispose(list);
 	terminate_test_suite();
 
